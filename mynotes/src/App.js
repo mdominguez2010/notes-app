8971be1726330db7c2ce5f55850a27c59622f1f8
Marcos Dominguez <mdominguez2010@yahoo.com>
2023-01-19 20:42:56 -0800

    header body templating

--- mynotes/src/App.js
+++ mynotes/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import './App.css'
+import Header from './components/Header'
+import Body from './components/Body'
 
 function App() {
 	return (
     	<div className="App">
-    		My Project
+    		<Header />
+			<Body />
     	</div>
-	);
+	)
 }
 
-export default App;
+export default App
